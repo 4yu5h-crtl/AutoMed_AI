ccd2f0d9f3654dff99f1bb289bd318981635471d
Ayush Chintalwar <ayush.chintalwar1234@gmail.com>
2025-11-29 10:15:04 +0530

    Update model_selection_agent.py

--- backend/agents/model_selection_agent.py
+++ backend/agents/model_selection_agent.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv, find_dotenv
 from backend.logger import send_log
 
 # IMPORTANT: Configure GEMINI KEY before running
-genai.configure(api_key="AIzaSyBaY8lV5seyGjvJ6ScMvZ6h7jfGuh5VbAU")
+genai.configure(api_key="your-api-key")
 load_dotenv(find_dotenv(), override=False)
 model_selector = genai.GenerativeModel("gemini-2.5-flash")
 
@@ -66,3 +66,4 @@ def model_selection_agent_node(state):
     send_log("model_selector", "Finished.")
 
     return state
+
